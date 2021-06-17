fb513dfcf03df0be5811936aaceb2f07a7e98d16
DanilaSuvorov <44905098+DanilaSuvorov@users.noreply.github.com>
2021-06-17 22:08:26 +0300

    Update Change_light.ino

--- Change_light.ino
+++ Change_light.ino
@@ -1,14 +1,8 @@
 #define foto 0
 #define led 7
-#include <Stepper.h>
-
-const int stepsPerRevolution = 100;
-
-Stepper myStepper(stepsPerRevolution, 8,9,10,11);
 
 void setup()
 {
- myStepper.setSpeed(30);
  Serial.begin(9600);
  pinMode(led, OUTPUT);
  
@@ -20,11 +14,6 @@ void loop()
  
 int val = (analogRead(foto));
 Serial.println(val);
-if(val > 50){
-  myStepper.step(100);
-  }else{
-    myStepper.step(-100);
-    }
 val = constrain (val, 10, 100);
 int Level = map (val ,100 ,10, 0 , 255);
 analogWrite (led, Level);
