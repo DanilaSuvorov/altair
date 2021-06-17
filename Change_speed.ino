87241721b9f285cf4ac40bdd0768cd2b013ec761
DanilaSuvorov <44905098+DanilaSuvorov@users.noreply.github.com>
2021-06-17 22:07:20 +0300

    Add files via upload

---
+++ Change_speed.ino
@@ -0,0 +1,31 @@
+#include <Stepper.h>
+
+const int stepsPerRevolution = 100;
+
+Stepper myStepper(stepsPerRevolution, 8,9,10,11); //подключение к пинам 8…11 на Ардуино
+
+void setup() {
+
+myStepper.setSpeed(50); //установка скорости вращения ротора
+
+Serial.begin(9600);
+
+}
+
+void loop() {
+
+//Функция ожидает, пока поступит команда, преобразовывает текст и подает сигнал на двигатель для его вращения на указанное число шагов.
+
+Serial.println("Move right"); //по часовой стрелке
+
+myStepper.step(stepsPerRevolution);
+
+delay(1000);
+
+Serial.println("Move left"); //против часовой стрелки
+
+myStepper.step(-stepsPerRevolution);
+
+delay(1000);
+
+}
